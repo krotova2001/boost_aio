1e7309f2b2cdaead972de113389c2e2e038f8069
George Krotov <zvuk24@gmail.com>
2023-03-26 22:26:52 +0700

    ап

--- Server.cpp
+++ Server.cpp
@@ -1,112 +1,53 @@
-﻿#include <iostream>
-#include "Logger.h" // класс для записи лога
-#include <string>
-#include <boost/asio.hpp>
-#include <boost/bind.hpp>  
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp> 
-#include <boost/array.hpp>
+﻿#include <boost/asio.hpp>
+#include <iostream>
+#include <memory>
+#include <optional>
 
-#pragma warning(disable : 4996)
-
-using boost::asio::ip::tcp;
-
-std::string make_daytime_string()
-{
-    using namespace std; // For time_t, time and ctime;
-    time_t now = time(0);
-    return ctime(&now);
-}
-
-class tcp_connection
-    : public boost::enable_shared_from_this<tcp_connection>
+//класс одного соединения
+class session : public std::enable_shared_from_this<session>
 {
 public:
-    typedef boost::shared_ptr<tcp_connection> pointer;
+    //конструктор по умолчанию, принимающий наш сокет
+    session(boost::asio::ip::tcp::socket&& socket) : socket(std::move(socket)) {}
 
-    static pointer create(boost::asio::io_context& io_context)
-    {
-        return pointer(new tcp_connection(io_context));
-    }
-
-    tcp::socket& socket()
-    {
-        return socket_;
-    }
-
+    //читаем данные из буфера, пока не встретим знак перевода строки
     void start()
     {
-        message_ = make_daytime_string();
-
-        boost::asio::async_write(socket_, boost::asio::buffer(message_),
-            boost::bind(&tcp_connection::handle_write, shared_from_this(),
-                boost::asio::placeholders::error,
-                boost::asio::placeholders::bytes_transferred));
+        boost::asio::async_read_until(socket, streambuf, '\n', [self = shared_from_this()](boost::system::error_code error, std::size_t bytes_transferred) { std::cout << std::istream(&self->streambuf).rdbuf(); });
     }
 
 private:
-    tcp_connection(boost::asio::io_context& io_context)
-        : socket_(io_context)
-    {
-    }
-
-    void handle_write(const boost::system::error_code& /*error*/,
-        size_t /*bytes_transferred*/)
-    {
-    }
-
-    tcp::socket socket_;
-    std::string message_;
+    boost::asio::ip::tcp::socket socket;
+    boost::asio::streambuf streambuf;
 };
 
-class tcp_server
+//класс сервера
+class server
 {
 public:
-    tcp_server(boost::asio::io_context& io_context)
-        : io_context_(io_context),
-        acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
+    server (boost::asio::io_context& io_context, std::uint16_t port) : io_context(io_context), acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {}
+
+    //начинаем принимать входящие подключения
+    void async_accept()
     {
-        start_accept();
+        auto socket = std::make_shared<boost::asio::ip::tcp::socket>(io_context);
+        acceptor.async_accept(*socket, [&, socket](boost::system::error_code error) 
+            {
+                std::make_shared<session>(std::move(*socket))->start();
+                async_accept();
+            });
     }
 
 private:
-    void start_accept()
-    {
-        tcp_connection::pointer new_connection =
-            tcp_connection::create(io_context_);
-
-        acceptor_.async_accept(new_connection->socket(),
-            boost::bind(&tcp_server::handle_accept, this, new_connection,
-                boost::asio::placeholders::error));
-    }
-
-    void handle_accept(tcp_connection::pointer new_connection,
-        const boost::system::error_code& error)
-    {
-        if (!error)
-        {
-            new_connection->start();
-        }
-
-        start_accept();
-    }
-
-    boost::asio::io_context& io_context_;
-    tcp::acceptor acceptor_;
+    boost::asio::io_context& io_context;
+    boost::asio::ip::tcp::acceptor acceptor;
 };
 
 int main()
 {
-    try
-    {
-        boost::asio::io_context io_context;
-        tcp_server server(io_context);
-        io_context.run();
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-
+    boost::asio::io_context io_context;
+    server srv(io_context, 15000); // создаем сервер и сокет на порту 15000
+    srv.async_accept(); // запускаем асинхронно при каждой новой сессии
+    io_context.run();
     return 0;
 }
